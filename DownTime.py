4e3cb81870f34872cdf066748c48dc76a0462e11
p4tino <kyleblatter@gmail.com>
2014-12-14 21:07:51 -0800

    Initial commit.

---
+++ DownTime.py
@@ -0,0 +1,12 @@
+#! /usr/bin/python
+# Basic countdown timer script. Reads a file containing sample dates and it
+# tells how much time before and after the dates specified in the script.
+
+import sys
+import argparse
+
+def main():
+    print 'main'
+
+if __name__ == "__main__":
+    sys.exit(main())
